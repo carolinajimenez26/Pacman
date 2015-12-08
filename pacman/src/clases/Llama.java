a02c42e02a8ba255f5b323edfa61e1ce63874bbe
Carolina <krito_j_g@utp.edu.co>
2015-12-08 10:45:16 -0500

    Dic 08 del 2015 h:10:45am

--- pacman/src/clases/Llama.java
+++ pacman/src/clases/Llama.java
@@ -45,21 +45,28 @@ public class Llama extends movimiento{
             for(int i = 1; i <= 3; i++){
                 if(control.m.getElemento(p.getFirst(), p.getSecond()) != 1){//if(t.matriz[Y/40][(X-(40*i))/40] != 1)
                     if(control.m.getElemento(p.getFirst() - i, p.getSecond()) == 2){
-                        control.v.llamita.setBounds(X-(40*i), Y, 40, 40);
-                        setX(X-i);//la llamita debe saber donde se encuentra
+                        control.v.llamita.setBounds(p.getFirst() - (40*i), p.getSecond(), 40, 40);
+                        setX(p.getFirst() - i);//la llamita debe saber donde se encuentra
                         control.v.llamita.setVisible(true);
-                        control.v.eliminar(X-i, Y);//eliminar(Y/40, (X-(40*i))/40);
-                        control.m.QuitaElemento(X-i, Y);
-                        Thread.sleep(100);
+                        control.v.eliminar(p.getSecond()/40, (p.getFirst() - (40*i))/40);//eliminar(Y/40, (X-(40*i))/40);
+                        control.m.QuitaElemento(p.getFirst() - i, p.getSecond());
+                        try {
+                            Thread.sleep(100);
+                        } catch (InterruptedException ex) {
+                            Logger.getLogger(Llama.class.getName()).log(Level.SEVERE, null, ex);
+                        }
                     }else{
-                        control.v.llamita.setBounds(X-(40*i), Y, 40, 40);
+                        control.v.llamita.setBounds(p.getFirst() - (40*i), p.getSecond(), 40, 40);
                         control.v.llamita.setVisible(true);
-                        Thread.sleep(100);
+                        try {
+                            Thread.sleep(100);
+                        } catch (InterruptedException ex) {
+                            Logger.getLogger(Llama.class.getName()).log(Level.SEVERE, null, ex);
+                        }
                     }
-                }else{
-                    break;
-                }
-                
+                }else break;
+            
+            }
             try {
                 Thread.sleep(getVelocidad());
             } catch (InterruptedException ex) {
@@ -69,82 +76,104 @@ public class Llama extends movimiento{
 
     @Override
     public void moveRight() {
-        if(actualiza() == 39){//derecha
-                            juego.llamita.setImage("/imagenes/llama.png");
-                            for(int i = 1; i <= 3; i++){
-                                if(t.matriz[Y/40][(X+(40*i))/40] != 1){
-                                    if(t.matriz[Y/40][(X+(40*i))/40]== 2){//si hay un fantasma
-                                        juego.llamita.setBounds(X+(40*i), Y, 40, 40);
-                                        juego.llamita.setVisible(true);
-                                        juego.eliminar(Y/40,(X+(40*i))/40);
-                                        Thread.sleep(100);
-                                    }else{
-                                        juego.llamita.setBounds(X+(40*i), Y, 40, 40);
-                                        juego.llamita.setVisible(true);
-                                        Thread.sleep(100);
-                                    }
-                                    
-                                }
-                                else break;
-                            }
-                            
-                            Thread.sleep(getVelocidad());
+        Pair p = control.m.getCarro();
+        control.v.llamita.setImage("/imagenes/llama.png");
+        
+            for(int i = 1; i <= 3; i++){
+                if(control.m.getElemento(p.getFirst(), p.getSecond()) != 1){
+                    if(control.m.getElemento(p.getFirst(), p.getSecond() + i) == 2){
+                        control.v.llamita.setBounds(p.getFirst() + (40*i), p.getSecond(), 40, 40);
+                        control.v.llamita.setVisible(true);
+                        control.v.eliminar(p.getSecond()/40, (p.getFirst() + (40*i)/40));//juego.eliminar(Y/40,(X+(40*i))/40);
+                        try {
+                            Thread.sleep(100);
+                        } catch (InterruptedException ex) {
+                            Logger.getLogger(Llama.class.getName()).log(Level.SEVERE, null, ex);
                         }
-                        
+                    }else{
+                        control.v.llamita.setBounds(p.getFirst() + (40*i), p.getSecond(), 40, 40);
+                        control.v.llamita.setVisible(true);
+                        try {
+                            Thread.sleep(100);
+                        } catch (InterruptedException ex) {
+                            Logger.getLogger(Llama.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                    }
+                }else break;
+            }
+            try {
+                Thread.sleep(getVelocidad());
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Llama.class.getName()).log(Level.SEVERE, null, ex);
+            }
     }
-
+                
     @Override
     public void moveDown() {
-        if(actualiza()==40){//abajo
-                            juego.llamita.setImage("/imagenes/llama_down.png");
-                            for(int i = 1; i <= 3; i++){
-                                if(t.matriz[(Y+(40*i))/40][X/40] != 1){
-                                    if(t.matriz[(Y+(40*i))/40][X/40]== 2){//si hay un fantasma
-                                        juego.llamita.setBounds(X, Y+(40*i), 40, 40);
-                                        juego.llamita.setVisible(true);
-                                        juego.eliminar((Y+(40*i))/40,X/40);
-                                        Thread.sleep(100);
-                                    }else{
-                                        juego.llamita.setBounds(X, Y+(40*i), 40, 40);
-                                        juego.llamita.setVisible(true);
-                                        Thread.sleep(100);
-                                    }
-                                }
-                                else break;
-                            }
-                            
-                            Thread.sleep(getVelocidad());
+        Pair p = control.m.getCarro();
+        control.v.llamita.setImage("/imagenes/llama_down.png");
+            
+            for(int i = 1; i <= 3; i++){
+                if(control.m.getElemento(p.getFirst(), p.getSecond()) != 1){
+                    if(control.m.getElemento(p.getFirst() + i, p.getSecond()) == 2){
+                        control.v.llamita.setBounds(p.getFirst(), p.getSecond() + (40*i), 40, 40);
+                        control.v.llamita.setVisible(true);
+                        control.v.eliminar((p.getSecond() + (40*i))/40,p.getFirst()/40);
+                        try {
+                            Thread.sleep(100);
+                        } catch (InterruptedException ex) {
+                            Logger.getLogger(Llama.class.getName()).log(Level.SEVERE, null, ex);
                         }
+                    }else{
+                        control.v.llamita.setBounds(p.getFirst(), p.getSecond() + (40*i), 40, 40);
+                        control.v.llamita.setVisible(true);
+                        try {
+                            Thread.sleep(100);
+                        } catch (InterruptedException ex) {
+                            Logger.getLogger(Llama.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                    }
+                }else break;
+            }
+        try {
+            Thread.sleep(getVelocidad());
+        } catch (InterruptedException ex) {
+            Logger.getLogger(Llama.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     @Override
     public void moveUp() {
-        if(actualiza() == 38){//arriba
-                            juego.llamita.setImage("/imagenes/llama_up.png");
-                            for(int i = 1; i <= 3; i++){
-                                if(t.matriz[(Y-(40*i))/40][X/40] != 1){
-                                    if(t.matriz[(Y-(40*i))/40][X/40]== 2){//si hay un fantasma
-                                        juego.llamita.setBounds(X, Y-(40*i), 40, 40);
-                                        juego.llamita.setVisible(true);
-                                        juego.eliminar((Y-(40*i))/40,X/40);
-                                        Thread.sleep(100);
-                                    }else{
-                                        juego.llamita.setBounds(X, Y-(40*i), 40, 40);
-                                        juego.llamita.setVisible(true);
-                                        Thread.sleep(100);
-                                    }
-                                    
-                                }
-                                
-                                else break;
-                            }
-                            
-                            Thread.sleep(getVelocidad());
+        Pair p = control.m.getCarro();
+        control.v.llamita.setImage("/imagenes/llama_up.png");
+            
+            for(int i = 1; i <= 3; i++){
+                if(control.m.getElemento(p.getFirst(), p.getSecond()) != 1){
+                    if(control.m.getElemento(p.getFirst() + i, p.getSecond()) == 2){
+                        control.v.llamita.setBounds(p.getFirst(), p.getSecond() - (40*i), 40, 40);
+                        control.v.llamita.setVisible(true);
+                        control.v.eliminar((p.getSecond()-(40*i))/40, p.getFirst()/40);
+                        try { 
+                            Thread.sleep(100);
+                        } catch (InterruptedException ex) {
+                            Logger.getLogger(Llama.class.getName()).log(Level.SEVERE, null, ex);
                         }
-                        
+                    }else{
+                        control.v.llamita.setBounds(p.getFirst(), p.getSecond()-(40*i), 40, 40);
+                        control.v.llamita.setVisible(true);
+                        try {
+                            Thread.sleep(100);
+                        } catch (InterruptedException ex) {
+                            Logger.getLogger(Llama.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                    }
+                }else break;
+            }
+        try {
+            Thread.sleep(getVelocidad());
+        } catch (InterruptedException ex) {
+            Logger.getLogger(Llama.class.getName()).log(Level.SEVERE, null, ex);
+        }             
     }
     
-    
-
-
 }
